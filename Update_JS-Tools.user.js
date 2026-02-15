82a02a6835cf4c24370734e61f92a32ca60014af
Personwritep <89448434+personwritep@users.noreply.github.com>
2026-02-15 16:46:46 +0900

    Update version to 0.2 and enhance download functionality
    Updated version number and modified download logic for GitHub links.

--- Update_JS-Tools.user.js
+++ Update_JS-Tools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Update JS-Tools
 // @namespace        http://tampermonkey.net/
-// @version        0.1
+// @version        0.2
 // @description        スクリプトツール一覧表からスクリプトツールをアップデート
 // @author        Ameba Blog User
 // @match        https://ameblo.jp/personwritep/entry-12762321136.html
@@ -23,46 +23,36 @@ monitor.observe(target, {childList: true});
 function main(){
     let all_td=document.querySelectorAll('td');
     for(let k=0; k<all_td.length; k++){
-        if(all_td[k].textContent=="♻"){
-            all_td[k].style.cursor='pointer';
-            all_td[k].onclick=function(){
-                let link=all_td[k-1].querySelector('a');
+        if(all_td[k].textContent=="GitHub"){
+            all_td[k].oncontextmenu=function(){ //「GitHub」を「右Click」
+                let link=all_td[k].querySelector('a');
                 if(link){
-                    let link_url=link.getAttribute('href');
-                    let parts=link_url.split('/');
-                    let tool_name=parts[parts.length-1];
-                    let download_url=link_url +'/raw/main/'+ tool_name +'.user.js';
-                    window.open(download_url, '_blank', );
-                }
-            }}}
+                    let tool_name;
+                    let download_url;
+                    let link_url;
+                    let link_href=link.getAttribute('href');
+
+                    let guery=link_href.split('?')[1];
+                    if(guery){
+                        tool_name=guery.toString();
+                        link_url=link_href.split('?')[0];
+                        download_url=link_url +'/raw/main/'+ tool_name +'.user.js';
+                        window.open(download_url, '_blank', ); }
+                    else{
+                        let parts=link_href.split('/');
+                        tool_name=parts[parts.length-1];
+                        download_url=link_href +'/raw/main/'+ tool_name +'.user.js';
+                        window.open(download_url, '_blank', ); }
+                }}}}
 
 } // main()
 
 
-
-
 /*
-// 一覧表のGitHubのリンクが適正かをチェックするスクリプト
+1個のリポジトリに複数のスクリプトツールを纏めている場合に、リポジトリ名とツール名が異なり
+自動アップデートの対象ツールが判定出来ない事になります。
 
-let text='';
-
-setTimeout(()=>{
-
-for(let k=0; k<all_td.length; k++){
-    if(all_td[k].textContent=="♻"){
-        let pre_td=all_td[k].previousElementSibling;
-        let link=pre_td.querySelector('a');
-        if(link){
-            let link_url=link.getAttribute('href');
-            let parts=link_url.split('/');
-            let tool_name=parts[parts.length-1];
-
-           text+=tool_name+'|';
-        }}}
-
-alert(text); // リンクが不適切な場合はツール名に「main」が混入する
-
-}, 1000);
-
+これを回避するために、混成のリポジトリでは、クエリ文字列でツール名を追記したリンクを用意し
+通常にリンクの「左Click」ではリポジトリを表示し、リンクの「右Click」では、このツールが機能し
+スクリプトツールの「アップデートリンク」を開く様にします。 （2026年2月 このリンクに変更）
 */
-
