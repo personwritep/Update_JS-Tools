08cc2e9842b169252575ef4b99d88eb3de361c8e
Personwritep <89448434+personwritep@users.noreply.github.com>
2026-04-19 06:01:51 +0900

    Update Update_JS-Tools.user.js

--- Update_JS-Tools.user.js
+++ Update_JS-Tools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Update JS-Tools
 // @namespace        http://tampermonkey.net/
-// @version        0.2
+// @version        0.3
 // @description        スクリプトツール一覧表からスクリプトツールをアップデート
 // @author        Ameba Blog User
 // @match        https://ameblo.jp/personwritep/entry-12762321136.html
